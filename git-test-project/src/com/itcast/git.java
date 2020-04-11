f06dbf47d5aafda5b261bf7890ae666e559020ae
admin <zhangsan>
2020-04-11 13:29:25 +0800

    打印

---
+++ git-test-project/src/com/itcast/git.java
@@ -0,0 +1,18 @@
+package com.itcast;
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+
+    }
+
+    public void aa(){
+        System.out.printf("aaa");
+    }
+}
